2212fed0ce1ddb62c8df321e06de29bc8150b2b0
viowla <55015538+viowla@users.noreply.github.com>
2020-12-06 21:33:06 +0600

    add common service dependency

--- src/test/java/kz/iitu/commonservice/CommonServiceApplicationTests.java
+++ src/test/java/kz/iitu/commonservice/CommonServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package kz.iitu.commonservice;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
